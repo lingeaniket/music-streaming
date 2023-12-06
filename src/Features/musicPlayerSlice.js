27495273f84a7f06c55a630ef0fd20dc9ee0e7fe
lingeaniket <linge.aniket.10@gmail.com>
2023-12-06 15:29:29 +0530

    Slice updated

---
+++ src/Features/musicPlayerSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const playerSlice = createSlice({
+    name: "player",
+    initialState: {
+        currentSong: {
+          downloadUrl : ""
+        },
+        nextPlaylist: [],
+    },
+    reducers: {
+        playNewSong: (state, action) => {
+            state.currentSong = action.payload.song;
+        },
+        addSongToQueue: (state, action) => {
+            state.nextPlaylist.push(action.payload.song);
+        },
+        playAlbum: (state, action) => {
+            state.currentSong = action.payload.song;
+            state.nextPlaylist = action.payload.playlist;
+        },
+    },
+});
+
+export const { playNewSong, addSongToQueue, playAlbum } = playerSlice.actions;
+
+export default playerSlice.reducer;
