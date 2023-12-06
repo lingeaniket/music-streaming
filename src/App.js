985486a3acb9daf5a29bb1912c6db391878d7f37
lingeaniket <linge.aniket.10@gmail.com>
2023-12-06 12:06:23 +0530

    App updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./Components/Home/Home";
+import LandingPage from "./Components/LandingPage/LandingPage";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Home />,
+        children: [
+            {
+                path: "/",
+                element: <LandingPage/>
+            }
+        ]
     },
 ]);
 
