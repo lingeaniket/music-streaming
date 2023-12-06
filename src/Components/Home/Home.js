6de58ae134da8a7e4b29950955c54252f4e798a6
lingeaniket <linge.aniket.10@gmail.com>
2023-12-06 12:07:07 +0530

    Home.js updated

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import Sidebar from "../Sidebar/Sidebar";
 import Footer from "../Footer/Footer";
+import { Outlet } from "react-router-dom";
 
 const Home = () => {
     useEffect(() => {}, []);
@@ -8,7 +9,19 @@ const Home = () => {
         <div className="mainContainer">
             <div className="gridMain">
                 <Sidebar />
-                <div className="main-content">main Content</div>
+                <div className="main-content">
+                <div
+            className="scrollHider"
+            style={{
+                border: "1px solid white",
+                padding: "8px",
+                margin: "0px 8px",
+            }}
+        >
+
+                    <Outlet />
+        </div>
+                </div>
                 <Footer />
             </div>
         </div>
