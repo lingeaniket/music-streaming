8fd6ab3cc09ce59ec91aaf5fd197955b0f0b2bac
lingeaniket <linge.aniket.10@gmail.com>
2023-12-06 15:28:10 +0530

    audioPlayer updated

--- src/Components/Footer/AudioPlayer.js
+++ src/Components/Footer/AudioPlayer.js
@@ -1,13 +1,16 @@
 // AudioPlayer.js
 import React, { useState, useRef, useEffect } from "react";
 import "./audioPlayer.css"; // Import the CSS file for styling
+import { useSelector } from "react-redux";
 
-const AudioPlayer = ({ src }) => {
+const AudioPlayer = () => {
+    const [currentSong, setCurrentSong] = useState("");
+    const [currentSongDetails, setCurrentSongDetails] = useState("");
     const [isPlaying, setIsPlaying] = useState(false);
     // eslint-disable-next-line
-    const [volume, setVolume] = useState(1);
+    const [volume, setVolume] = useState(0.5);
     const [currentTime, setCurrentTime] = useState(0);
-    const audioRef = useRef(new Audio(src));
+    const audioRef = useRef(new Audio(currentSong));
     const isSeeking = useRef(false); // Use a ref to track whether the user is currently seeking
 
     const togglePlay = () => {
@@ -22,12 +25,19 @@ const AudioPlayer = ({ src }) => {
     const handleVolumeChange = (e) => {
         const newVolume = parseFloat(e.target.value);
         setVolume(newVolume);
+        handleVolumeOverlay(newVolume * 100);
         audioRef.current.volume = newVolume;
     };
 
     const handleOverlay = (percent) => {
         document.getElementById("seek-range").style.background = `linear-gradient(90deg, white ${percent}%, gray 0)`;
         document.getElementById("overlay-div").style.width = `calc(${percent}% + ${percent > 50 ? "0px" : percent < 1 ? "0px" : "5px"})`;
+    };
+    const handleVolumeOverlay = (percent) => {
+        document.getElementById("seek-volume").style.background = `linear-gradient(90deg, white ${percent}%, gray 0)`;
+        document.getElementById("overlay-volume-div").style.width = `calc(${percent}% + ${
+            percent > 50 ? "0px" : percent < 1 ? "0px" : "5px"
+        })`;
     };
 
     const handleTimeUpdate = () => {
@@ -62,31 +72,152 @@ const AudioPlayer = ({ src }) => {
         setCurrentTime(seekTime);
     };
 
+    const selectedSong = useSelector((state) => state.player.currentSong);
+
+    useEffect(() => {
+        console.log(selectedSong);
+        setCurrentSong(selectedSong?.downloadUrl[2]?.link);
+        setCurrentSongDetails(selectedSong);
+        setTimeout(() => {
+            if(selectedSong?.downloadUrl){
+
+                audioRef.current.play();
+                setIsPlaying(true);
+            }
+        }, 200);
+        handleVolumeOverlay(volume*100)
+        handleOverlay(0);
+    }, [selectedSong]);
+
     return (
-        <div>
-            <audio id="audio-player" ref={audioRef} src={src} />
-            <div className="audio-container">
-                <button onClick={togglePlay}>{isPlaying ? "Pause" : "Play"}</button>
-                <div className="range-container">
-                    <div className="time-stamp">{formatTime(currentTime)}</div>
-                    <div className="range-cont">
-                        <input
-                            type="range"
-                            min="0"
-                            id="seek-range"
-                            className="range-input"
-                            max={audioRef.current.duration ? audioRef.current.duration : 0}
-                            step="0.01"
-                            value={currentTime}
-                            onInput={handleInputSeekChange}
-                            onMouseUp={handleInputSeek}
+        <>
+            <div
+                style={{
+                    width: "30%",
+                    minWidth: "180px",
+                }}
+            >
+                <div
+                    style={{
+                        display: "flex",
+                        padding: "10px",
+                    }}
+                >
+                    <div
+                        style={{
+                            width: "60px",
+                            aspectRatio: "1/1",
+                        }}
+                    >
+                        <img
+                            src={currentSongDetails?.image ? currentSongDetails?.image[2].link : ""}
+                            alt=""
+                            style={{
+                                verticalAlign: "middle",
+                                width: "100%",
+                            }}
                         />
-                        <div className="overlayDiv" id="overlay-div"></div>
                     </div>
-                    <div className="time-stamp">{formatTime(audioRef.current.duration)}</div>
+                    <div
+                        style={{
+                            paddingLeft: "16px",
+                            width: "calc(100% - 60px)",
+                            display: "flex",
+                            flexDirection: "column",
+                            justifyContent: "center",
+                        }}
+                    >
+                        <h4
+                            style={{
+                                textOverflow: "ellipsis",
+                                padding: 0,
+                                margin: 0,
+                                overflow: "hidden",
+                                lineHeight: 1.4,
+                                fontSize: "13px",
+                                whiteSpace: "nowrap",
+                            }}
+                        >
+                            {currentSongDetails?.name}
+                        </h4>
+                        <div
+                            style={{
+                                textOverflow: "ellipsis",
+                                overflow: "hidden",
+                                lineHeight: 1.4,
+                                fontSize: "13px",
+                                whiteSpace: "nowrap",
+                            }}
+                        >
+                            {currentSongDetails?.primaryArtists}
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+            <div
+                style={{
+                    width: "40%",
+                    maxWidth: "722px",
+                }}
+            >
+                <div>
+                    <audio id="audio-player" ref={audioRef} src={currentSong} />
+                    <div className="audio-container">
+                        <button onClick={togglePlay}>{isPlaying ? "Pause" : "Play"}</button>
+                        <div className="range-container">
+                            <div className="time-stamp">{formatTime(currentTime)}</div>
+                            <div className="range-cont">
+                                <input
+                                    type="range"
+                                    min="0"
+                                    id="seek-range"
+                                    className="range-input"
+                                    max={audioRef.current.duration ? audioRef.current.duration : 0}
+                                    step="0.01"
+                                    value={currentTime}
+                                    onInput={handleInputSeekChange}
+                                    onMouseUp={handleInputSeek}
+                                />
+                                <div className="overlayDiv" id="overlay-div"></div>
+                            </div>
+                            <div className="time-stamp">{formatTime(audioRef.current.duration)}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div
+                style={{
+                    width: "30%",
+                    minWidth: "180px",
+                }}
+            >
+                <div
+                    style={{
+                        display: "flex",
+                        alignItems: "center",
+                    }}
+                >
+                    <div>Volume</div>
+                    <div style={{
+                        paddingLeft: '16px'
+                    }}>
+                        <div className="range-cont">
+                            <input
+                                type="range"
+                                min="0"
+                                id="seek-volume"
+                                className="range-input"
+                                max={1}
+                                step="0.01"
+                                value={volume}
+                                onChange={handleVolumeChange}
+                            />
+                            <div className="overlayDiv" id="overlay-volume-div"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
