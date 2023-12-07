c3d8e73fe45e6a2b922b0148cde8a7da9704f642
lingeaniket <linge.aniket.10@gmail.com>
2023-12-07 16:12:05 +0530

    Footer updated

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,11 +1,7 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import React from "react";
 import AudioPlayer from "./AudioPlayer";
 
 const Footer = () => {
-    const [musicdata, setMusicData] = useState("");
-
     return (
         <div className="footer">
             <div
