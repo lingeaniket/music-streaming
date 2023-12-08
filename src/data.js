4d45839e3297169f6e433d21da0b9b253d0cd8e8
lingeaniket <linge.aniket.10@gmail.com>
2023-12-08 17:14:18 +0530

    data updated

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-// to get auth code 
+// to get auth code
 
 // var client_id = "7bb900aae1c544518c151201106d7bba";
 //         var client_secret = "efd828bfe750468d826269be822295ce";
@@ -26,3 +26,29 @@
 //                 console.error("Error:", error.response.data);
 //             });
 
+//yiu may like for album
+// https://www.jiosaavn.com/api.php?__call=reco.getAlbumReco&api_version=4&_format=json&_marker=0&ctx=wap6dot0&albumid=49986025
+
+// related playlist
+// https://www.jiosaavn.com/api.php?__call=reco.getPlaylistReco&api_version=4&_format=json&_marker=0&ctx=wap6dot0&listid=158226045
+
+//new releases
+// https://www.jiosaavn.com/api.php?__call=content.getAlbums&api_version=4&_format=json&_marker=0&n=50&p=1&ctx=wap6dot0
+
+//new releases by language
+// https://www.jiosaavn.com/api.php?__call=content.getAlbums&api_version=4&_format=json&_marker=0&n=50&p=1&ctx=wap6dot0&languages=tamil
+
+//get top albums of same year api with language
+// https://www.jiosaavn.com/api.php?__call=search.topAlbumsoftheYear&api_version=4&_format=json&_marker=0&ctx=wap6dot0&album_year=2023&album_lang=tamil
+
+//actors top
+// https://www.jiosaavn.com/api.php?__call=search.actorOtherTopSongs&api_version=4&_format=json&_marker=0&ctx=wap6dot0&actor_ids=458344&song_id=LwzoHAWj&language=tamil
+
+// artists top songs
+// https://www.jiosaavn.com/api.php?__call=search.artistOtherTopSongs&api_version=4&_format=json&_marker=0&ctx=wap6dot0&artist_ids=743307&song_id=LwzoHAWj&language=tamil
+
+//get footer details
+//https://www.jiosaavn.com/api.php?__call=webapi.getFooterDetails&language=tamil&api_version=4&_format=json&_marker=0
+
+//get lauch data
+//https://www.jiosaavn.com/api.php?__call=webapi.getLaunchData&api_version=4&_format=json&_marker=0&ctx=wap6dot0
