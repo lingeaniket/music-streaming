fcf1fb42711ef5dced55143bf898e8aa84087df1
lingeaniket <linge.aniket.10@gmail.com>
2023-12-08 17:17:31 +0530

    function updated

---
+++ src/Components/commonFunctions.js
@@ -0,0 +1,8 @@
+export const formatTime = (time) => {
+    if (time) {
+        const minutes = Math.floor(time / 60);
+        const seconds = Math.floor(time % 60);
+        return `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+    }
+    return "00:00";
+};
