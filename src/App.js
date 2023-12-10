b7cda719ee8315f35cbce2a9f5cc7c7234b0e680
lingeaniket <linge.aniket.10@gmail.com>
2023-12-10 18:22:55 +0530

    App.js updated

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Details from "./Components/Details/Details";
 import LandingPage from "./Components/LandingPage/LandingPage";
 
 import store from "./Store/store";
+import Search from "./Components/Search/Search";
 
 const router = createBrowserRouter([
     {
@@ -29,6 +30,10 @@ const router = createBrowserRouter([
                 path: "featured/:name/:id",
                 element: <Details type="playlist" />,
             },
+            {
+                path: 'search',
+                element: <Search/>
+            }
         ],
     },
 ]);
