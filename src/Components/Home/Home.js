3593f44ea952aa4ddb28f877752a770d54f78cf9
lingeaniket <linge.aniket.10@gmail.com>
2023-12-11 17:50:57 +0530

    Home.js updated

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,10 +3,13 @@ import { Outlet } from "react-router-dom";
 
 import Footer from "../Footer/Footer";
 import Sidebar from "../Sidebar/Sidebar";
+import { useSelector } from "react-redux";
 
 const Home = () => {
+    const selectedSong = useSelector((state) => state.player.currentSong);
+
     useEffect(() => {}, []);
-    
+
     return (
         <div className="mainContainer">
             <div className="gridMain">
@@ -18,12 +21,13 @@ const Home = () => {
                             // border: "1px solid white",
                             padding: "8px",
                             margin: "0px 8px",
+                            height: "100%",
                         }}
                     >
                         <Outlet />
                     </div>
                 </div>
-                <Footer />
+                {selectedSong.downloadUrl && <Footer />}
             </div>
         </div>
     );
