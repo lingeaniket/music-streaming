80d9ba3d6340d45e778f92306a003c291354f752
lingeaniket <linge.aniket.10@gmail.com>
2023-12-11 17:51:46 +0530

    PageButton updated

---
+++ src/Components/Search/ViewAll/PageButton.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const PageButton = ({ page, index, handleButtonPage }) => {
+    const handleClick = (e) => {
+        if (!Number.isNaN(e.target.innerText)) {
+            handleButtonPage(Number(e.target.innerText));
+        }
+    };
+    return (
+        <div className="vAll010"
+            key={index}
+            
+        >
+            <div className="vAll011"
+                key={index}
+                style={{
+                    background: `${page === index + 1 ? "green" : "white"}`,
+                    width: "50px",
+                    border: `2px solid ${page === index+1 ? "white": "black"}`,
+                    color: `${page === index + 1 ? "white" : "black"}`,
+                }}
+                onClick={handleClick}
+            >
+                {index + 1}
+            </div>
+        </div>
+    );
+};
+
+export default PageButton;
