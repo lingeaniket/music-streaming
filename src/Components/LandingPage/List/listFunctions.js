e0270d4de88e81d2e9f607fd7432a1fdaa07d269
lingeaniket <linge.aniket.10@gmail.com>
2023-12-15 15:24:44 +0530

    listFunction updated

--- src/Components/LandingPage/List/listFunctions.js
+++ src/Components/LandingPage/List/listFunctions.js
@@ -1,19 +1,18 @@
 import axios from "axios";
+
 export const getPlayListData = async (data) => {
     if (data.type === "album") {
         const albumSongsData = await axios.get(`https://saavn.me/albums?id=${data.id}`);
         const albumData = albumSongsData.data.data;
         const playerData = {
-            song: albumData.songs[0],
-            playlist: albumData.songs.length > 2 ? albumData.songs.slice(1, albumData.songCount) : albumData.songs[1],
+            song: albumData.songs[0].id,
+            playlist: albumData.songs.map((song) => song.id),
         };
         return playerData;
     } else if (data.type === "song") {
-        const albumSongsData = await axios.get(`https://saavn.me/songs?id=${data.id}`);
-        const albumData = albumSongsData.data.data;
         const playerData = {
-            song: albumData[0],
-            playlist: [],
+            song: data.id,
+            playlist: [data.id],
         };
 
         return playerData;
@@ -21,24 +20,18 @@ export const getPlayListData = async (data) => {
         const albumSongsData = await axios.get(`https://saavn.me/playlists?id=${data.id}`);
         const albumData = albumSongsData.data.data;
         const playerData = {
-            song: albumData.songs[0],
-            playlist: albumData.songs.length > 2 ? albumData.songs.slice(1, albumData.songCount) : albumData.songs[1],
+            song: albumData.songs[0].id,
+            playlist: albumData.songs.map((song) => song.id),
         };
 
         return playerData;
     } else if (data.type === "artist") {
         const artistData = await axios.get(`https://saavn.me/artists/${data.id}/songs?page=1`);
         const artistSongsData = artistData.data.data;
-        // console.log(artistSongsData)
         const playerData = {
-            song: artistSongsData.results[0],
-            playlist:
-                artistSongsData.results.length > 2
-                    ? artistSongsData.results.slice(1, artistSongsData.results.length)
-                    : artistSongsData.results[1],
+            song: artistSongsData.results[0].id,
+            playlist: artistSongsData.results.map((song) => song.id),
         };
-
-        console.log(playerData)
 
         return playerData;
     }
