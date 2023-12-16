d38dc3260e8b91e0228afd7cc33709837ed2376e
lingeaniket <linge.aniket.10@gmail.com>
2023-12-16 14:40:40 +0530

    Queue added

---
+++ src/Components/QueueSide/QueueList/QueueList.js
@@ -0,0 +1,41 @@
+import { faItunesNote } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const QueueList = ({ item }) => {
+    const [songData, setSongData] = useState({});
+    useEffect(() => {
+        const loadData = async (id) => {
+            const data = await axios.get(`https://saavn.me/songs?id=${id}`);
+            setSongData(data.data.data[0]);
+        };
+
+        if (item) {
+            loadData(item);
+        }
+    }, [item]);
+
+    return (
+        <div className="queue10">
+            <div className="queue11">
+                <div className="queue12">
+                    <FontAwesomeIcon icon={faItunesNote} size="2xs" />
+                </div>
+                <div className="queue13">
+                    <img src={songData.image ? songData.image[2].link : ""} alt="" />
+                </div>
+                <div className="queue14">
+                    <div>
+                        <h5 className="queue15">{songData.name ? songData.name : ""}</h5>
+                    </div>
+                    <div>
+                        <h5 className="queue16">{songData.primaryArtists}</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default QueueList;
---
+++ src/Components/QueueSide/QueueSide.js
@@ -0,0 +1,77 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import QueueList from "./QueueList/QueueList";
+import "./queue.css";
+import { toggleQueue } from "../../Features/musicPlayerSlice";
+
+const QueueSide = () => {
+    const dispatch = useDispatch();
+    const currentSong = useSelector((state) => state.player.currentSong);
+    const songIndex = useSelector((state) => state.player.songIndex);
+    const songQueue = useSelector((state) => state.player.songQueue);
+    // eslint-disable-next-line
+    const autoPlaylist = useSelector((state) => state.player.autoPlaylist);
+    // eslint-disable-next-line
+    const isAutoPlay = useSelector((state) => state.player.isAutoPlay);
+    // eslint-disable-next-line
+    const autoPlaylistIndex = useSelector((state) => state.player.autoPlaylistIndex);
+
+    const [currentSongDetails, setCurrentSongDetails] = useState({});
+
+    const handleQueueClose = () => {
+        dispatch(toggleQueue());
+    };
+
+    useEffect(() => {
+        const loadCurrentSong = async (id) => {
+            const data = await axios.get(`https://saavn.me/songs?id=${id}`);
+
+            setCurrentSongDetails(data.data.data[0]);
+        };
+
+        if (currentSong !== 0) {
+            loadCurrentSong(currentSong);
+        }
+    }, [currentSong]);
+
+    return (
+        <div className="queue01">
+            <div className="queue02">
+                <div className="queue17">
+                    <div className="queue18"></div>
+                    <div className="queue18 queue19">
+                        <div className="queue20" onClick={handleQueueClose}>
+                            X
+                        </div>
+                    </div>
+                </div>
+                <div className="queue03">
+                    <div>
+                        <div className="queue04">
+                            <img src={currentSongDetails.image ? currentSongDetails.image[2]?.link : ""} alt="" className="" />
+                        </div>
+                        <div>
+                            <h2 className="queue05">{currentSongDetails.name ? currentSongDetails.name.replace(/&quot;/g, '"') : ""}</h2>
+                            <p className="queue06">{currentSongDetails.primaryArtists}</p>
+                        </div>
+                    </div>
+                    <div className="queue07">
+                        <div>
+                            <h4 className="queue08">Next in queue</h4>
+                        </div>
+                        <div className="queue09">
+                            {songQueue
+                                .filter((q, ind) => ind > songIndex)
+                                .map((queue) => (
+                                    <QueueList key={queue} item={queue} />
+                                ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default QueueSide;
