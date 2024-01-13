072ebfdcae51bdbcd87fe396effeecf0526a0205
lingeaniket <linge.aniket.10@gmail.com>
2024-01-13 14:30:22 +0530

    store updated

--- src/Store/store.js
+++ src/Store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import musicPlayerSlice from "../Features/musicPlayerSlice";
+import userSlice from "../Features/userSlice";
 
 export default configureStore({
     reducer: {
         player: musicPlayerSlice,
+        user: userSlice,
     },
 });
