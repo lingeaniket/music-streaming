af00464c8b6833d8203ec30ff473f61d2b6efc31
lingeaniket <linge.aniket.10@gmail.com>
2024-01-13 14:30:43 +0530

    userSlice updated

---
+++ src/Features/userSlice.js
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userSlice = createSlice({
+    name: "user",
+    initialState: {
+        user: localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : "",
+        liked: localStorage.getItem("liked")
+            ? JSON.parse(localStorage.getItem("liked"))
+            : { songs: [], playlists: [], albums: [], artists: [] },
+        history: [],
+    },
+    reducers: {
+        addLiked: (state, action) => {
+            const { id, type } = action.payload;
+            if (state.liked[`${type}s`].findIndex((song) => song === id) === -1) {
+                state.liked[`${type}s`].unshift(id);
+            }
+            localStorage.setItem("liked", JSON.stringify(state.liked));
+        },
+        removeLiked: (state, action) => {
+            const { id, type } = action.payload;
+            let idx = -1;
+            if (
+                state.liked[`${type}s`].findIndex((song, ind) => {
+                    if (song === id) {
+                        idx = ind;
+                    }
+                    return song === id;
+                }) > -1
+            ) {
+                state.liked[`${type}s`].splice(idx, 1);
+            }
+            localStorage.setItem("liked", JSON.stringify(state.liked));
+        },
+    },
+});
+
+export const { addLiked, removeLiked } = userSlice.actions;
+
+export default userSlice.reducer;
