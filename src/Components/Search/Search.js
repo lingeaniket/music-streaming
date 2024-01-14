452f399d8b94a6a89b8b72be6b5f7e2ee230769f
lingeaniket <linge.aniket.10@gmail.com>
2024-01-14 18:50:07 +0530

    Search updated

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { useDispatch } from "react-redux";
 import React, { useEffect, useState } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import "./search.css";
 
 import { playAlbum } from "../../Features/musicPlayerSlice";
 import { getPlayListData } from "../LandingPage/List/listFunctions";
@@ -73,341 +74,97 @@ const Search = () => {
     }, []);
 
     return (
-        <div
-            style={{
-                padding: "22px",
-                position: "relative",
-            }}
-        >
-            <div
-                style={{
-                    position: "absolute",
-                    inset: 0,
-                }}
-            >
-                <Outlet />
-            </div>
-            <div
-                style={{
-                    display: "flex",
-                    alignItems: "center",
-                    padding: "8px 12px 8px 0",
-                    width: "100%",
-                    marginBottom: "33px",
-                }}
-            >
-                <div
-                    style={{
-                        display: "flex",
-                        justifyContent: "space-between",
-                        paddingRight: "12px",
-                        width: "100px",
-                    }}
-                >
+        <div className="srch01">
+            {/* <Outlet /> */}
+            <div className="app06 srch03 w-100">
+                <div className="app03 srch04">
                     <div>back</div>
                     <div>next</div>
                 </div>
-                <div
-                    style={{
-                        width: "35%",
-                        position: "relative",
-                    }}
-                >
+                <div className="w-35 pRel">
                     <input
                         type="text"
-                        style={{
-                            background: "transparent",
-                            border: "2px solid white",
-                            borderRadius: "999px",
-                            outline: "none",
-                            fontSize: "15px",
-                            color: "white",
-                            padding: "15px 10px 15px 40px",
-                            width: "364px",
-                        }}
+                        className="srch05"
                         value={searchKey}
                         onInput={handleInput}
                         placeholder="What do you want to listen to?"
                     />
-                    <div
-                        style={{
-                            position: "absolute",
-                            left: "25px",
-                            top: "50%",
-                            transform: "translate(-50%, -50%)",
-                        }}
-                    >
+                    <div className="srch06">
                         <FontAwesomeIcon icon={faMagnifyingGlass} size="sm" />
                     </div>
                 </div>
             </div>
             {isSearching ? (
                 <div>
-                    <div
-                        style={{
-                            display: "flex",
-                        }}
-                    >
+                    <div className="app01">
                         {searchData?.topQuery?.results.map((data) => (
-                            <div
-                                style={{
-                                    width: "50%",
-                                }}
-                            >
+                            <div className="w-50">
                                 <h2>Top Result</h2>
 
-                                <div
-                                    style={{
-                                        width: "100%",
-                                        display: "flex",
-                                        position: "relative",
-                                    }}
-                                >
-                                    <div
-                                        style={{
-                                            width: "40%",
-                                            marginBottom: "22px",
-                                        }}
-                                    >
-                                        <img
-                                            src={data.image[2].link}
-                                            alt=""
-                                            style={{
-                                                width: "100%",
-                                                borderRadius: "4px",
-                                                verticalAlign: "middle",
-                                            }}
-                                        />
+                                <div className="w-100 app01 pRel srch12">
+                                    <div className="w-40 srch07">
+                                        <img src={data.image[2].link} alt="" className="srch08" />
                                     </div>
-                                    <div
-                                        style={{
-                                            marginLeft: "22px",
-                                        }}
-                                    >
-                                        <h2
-                                            style={{
-                                                width: "100%",
-                                                textOverflow: "ellipsis",
-                                                wordWrap: "wrap",
-                                            }}
-                                        >
-                                            {data.title}
-                                        </h2>
+                                    <div className="srch09">
+                                        <h2 className="w-100 srch10">{data.title}</h2>
                                         <p>{data.description}</p>
-                                        <div
-                                            style={{
-                                                backgroundColor: "green",
-                                                width: "60px",
-                                                display: "flex",
-                                                height: "30px",
-                                                justifyContent: "center",
-                                                padding: "5px",
-                                                borderRadius: "999px",
-                                                alignItems: "center",
-                                                textTransform: "capitalize",
-                                                border: "2px solid white",
-                                            }}
-                                        >
-                                            {data.type}
-                                        </div>
+                                        <div className="srch11 app05">{data.type}</div>
                                     </div>
 
                                     {/* show only if parent hovered */}
-                                    <div
-                                        style={{
-                                            position: "absolute",
-                                            right: "30px",
-                                            bottom: "30px",
-                                        }}
-                                    >
-                                        <button
-                                            style={{
-                                                background: "white",
-                                                borderRadius: "999px",
-                                                width: "auto",
-                                                height: "auto",
-                                                padding: "10px 30px",
-                                                border: "none",
-                                                fontWeight: 600,
-                                                fontSize: "15px",
-                                                cursor: "pointer",
-                                            }}
-                                            onClick={handleTopSearch}
-                                        >
+                                    <div className="srch13">
+                                        <button className="srch14" onClick={handleTopSearch}>
                                             Play
                                         </button>
                                     </div>
                                 </div>
                             </div>
                         ))}
-                        <div
-                            style={{
-                                width: "50%",
-                                position: "relative",
-                            }}
-                        >
-                            <div
-                                style={{
-                                    display: "flex",
-                                    justifyContent: "space-between",
-                                    alignItems: "center",
-                                }}
-                            >
+                        <div className="w-50 pRel">
+                            <div className="app03 app06">
                                 <h2>Songs</h2>
-
-                                <div
-                                    style={{
-                                        display: "flex",
-                                        justifyContent: "flex-end",
-                                    }}
-                                >
-                                    <button
-                                        style={{
-                                            height: "30px",
-                                            padding: "5px 12px",
-                                            borderRadius: "999px",
-                                            border: "none",
-                                        }}
-                                        onClick={handleViewAll.bind({ type: "song" })}
-                                    >
+                                <div className="app10">
+                                    <button className="srch15" onClick={handleViewAll.bind({ type: "song" })}>
                                         View all
                                     </button>
                                 </div>
                             </div>
-
                             {searchData?.songs?.results?.map((song, index) => (
                                 <SongList song={song} index={index} type="song" mode="search" />
                             ))}
                         </div>
                     </div>
-                    <div>
-                        <div
-                            style={{
-                                display: "flex",
-                                justifyContent: "space-between",
-                                alignItems: "center",
-                            }}
-                        >
-                            <h2>Album</h2>
-                            <div
-                                style={{
-                                    display: "flex",
-                                    justifyContent: "flex-end",
-                                }}
-                            >
-                                <button
-                                    style={{
-                                        height: "30px",
-                                        padding: "5px 12px",
-                                        borderRadius: "999px",
-                                        border: "none",
-                                    }}
-                                    onClick={handleViewAll.bind({ type: "album" })}
-                                >
-                                    View all
-                                </button>
-                            </div>
-                        </div>
-                        <div
-                            style={{
-                                display: "flex",
-                                margin: "0 0 0 -22px",
-                            }}
-                        >
-                            {searchData?.albums?.results.map((album) => (
-                                <ListItem data={album} />
+                    {searchData.albums && (
+                        <>
+                            {["album", "artist", "playlist"].map((type) => (
+                                <div key={type}>
+                                    <div className="app03 app06">
+                                        <h2 className="srch18">{type}</h2>
+                                        <div
+                                            className="app10"
+                                            style={{
+                                                cursor: "pointer",
+                                            }}
+                                        >
+                                            <button className="srch15" onClick={handleViewAll.bind({ type })}>
+                                                View all
+                                            </button>
+                                        </div>
+                                    </div>
+                                    <div className="app01 srch16">
+                                        {searchData[`${type}s`]?.results?.map((album) => (
+                                            <ListItem data={album} />
+                                        ))}
+                                    </div>
+                                </div>
                             ))}
-                        </div>
-                    </div>
-                    <div>
-                        <div
-                            style={{
-                                display: "flex",
-                                justifyContent: "space-between",
-                                alignItems: "center",
-                            }}
-                        >
-                            <h2>Artists</h2>
-                            <div
-                                style={{
-                                    display: "flex",
-                                    justifyContent: "flex-end",
-                                }}
-                            >
-                                <button
-                                    style={{
-                                        height: "30px",
-                                        padding: "5px 12px",
-                                        borderRadius: "999px",
-                                        border: "none",
-                                    }}
-                                    onClick={handleViewAll.bind({ type: "artist" })}
-                                >
-                                    View all
-                                </button>
-                            </div>
-                        </div>
-                        <div
-                            style={{
-                                display: "flex",
-                                margin: "0 0 0 -22px",
-                            }}
-                        >
-                            {searchData?.artists?.results.map((album) => (
-                                <ListItem data={album} />
-                            ))}
-                        </div>
-                    </div>
-                    <div>
-                        <div
-                            style={{
-                                display: "flex",
-                                justifyContent: "space-between",
-                                alignItems: "center",
-                            }}
-                        >
-                            <h2>Playlists</h2>
-                            <div
-                                style={{
-                                    display: "flex",
-                                    justifyContent: "flex-end",
-                                }}
-                            >
-                                <button
-                                    style={{
-                                        height: "30px",
-                                        padding: "5px 12px",
-                                        borderRadius: "999px",
-                                        border: "none",
-                                    }}
-                                    onClick={handleViewAll.bind({ type: "playlist" })}
-                                >
-                                    View all
-                                </button>
-                            </div>
-                        </div>
-                        <div
-                            style={{
-                                display: "flex",
-                                margin: "0 0 0 -22px",
-                            }}
-                        >
-                            {searchData?.playlists?.results.map((album, i) => (
-                                <ListItem data={album} key={i} />
-                            ))}
-                        </div>
-                    </div>
+                        </>
+                    )}
                 </div>
             ) : (
                 <>
                     <h2>Trending</h2>
-                    <div
-                        style={{
-                            display: "flex",
-                            flexWrap: "wrap",
-                            margin: "0 0 0 -22px",
-                        }}
-                    >
+                    <div className="app01 srch16 srch17">
                         {topSearch.map((top, i) => (
                             <ListItem key={i} data={top} />
                         ))}
