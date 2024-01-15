089ddab7e716275fe540e060eb8eee2a1cd40505
lingeaniket <linge.aniket.10@gmail.com>
2024-01-15 16:57:16 +0530

    options added

--- src/Components/Options/Options.js
+++ src/Components/Options/Options.js
@@ -1,22 +1,56 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./options.css";
+import { useDispatch, useSelector } from "react-redux";
+import { addSongsToPlaylist, openPlaylist, updateCurrentdata } from "../../Features/newPlaylistSlice";
+import axios from "axios";
 
 const Options = ({ liked, data, handleLike, setoptions }) => {
+    console.log(data);
     const opref = useRef();
+    const myPlaylists = useSelector((state) => state.playlist.myPlaylists);
+
+    const [main, setMain] = useState(true);
+    const dispatch = useDispatch();
 
     const handleLiked = (e) => {
         setoptions(false);
         handleLike(e);
     };
 
+    async function handleAddToPlaylist() {
+        const currentid = this.id;
+        const { type, id } = data;
+        if (type === "album" || type === "playlist") {
+            const data = await axios.get(`https://saavn.me/${type}s?id=${id}`);
+            dispatch(addSongsToPlaylist({ id: currentid, songs: data.data.data.songs }));
+        } else if (type === "song") {
+            const data = await axios.get(`https://saavn.me/songs?id=${id}`);
+            dispatch(addSongsToPlaylist({ id: currentid, songs: data.data.data[0] }));
+        }
+        dispatch(updateCurrentdata(""));
+        setoptions(false);
+    }
+
     const stayInModel = (event) => {
         event.stopPropagation();
     };
+
+    function handleMode() {
+        setMain((prev) => !prev);
+    }
 
     const handleClick = (e) => {
         if (opref.current && !opref.current.contains(e.target)) {
             setoptions(false);
         }
+    };
+
+    const handleOpenNew = () => {
+        setoptions(false);
+        setTimeout(() => {
+            dispatch(updateCurrentdata(data));
+            dispatch(openPlaylist(true));
+        }, 0);
     };
 
     useEffect(() => {
@@ -42,12 +76,52 @@ const Options = ({ liked, data, handleLike, setoptions }) => {
                 background: "white",
             }}
         >
-            <div className="opt03" onClick={handleLiked}>
-                {liked ? "Remove from Library" : "Save to Library"}
-            </div>
-            <div className="opt03">Play Playlist Now</div>
-            <div className="opt03">Add to Queue</div>
-            <div className="opt03">Add to Playlist</div>
+            {main ? (
+                <>
+                    <div className="opt03" onClick={handleLiked}>
+                        {liked ? "Remove from Library" : "Save to Library"}
+                    </div>
+                    <div className="opt03">Play Playlist Now</div>
+                    <div className="opt03">Add to Queue</div>
+                    <div className="opt03" onClick={handleMode}>
+                        Add to Playlist
+                    </div>
+                </>
+            ) : (
+                <>
+                    <div className="opt03" onClick={handleMode}>
+                        Back
+                    </div>
+                    <div
+                        style={{
+                            border: "1px solid #e9e9e9 ",
+                            borderLeft: "none",
+                            borderRight: "none",
+                            margin: "5px 0",
+                            padding: "5px 0",
+                        }}
+                    >
+                        <div className="opt03" onClick={handleOpenNew}>
+                            <div>
+                                <span
+                                    style={{
+                                        fontSize: "16px",
+                                        paddingRight: "10px",
+                                    }}
+                                >
+                                    +
+                                </span>{" "}
+                                <span>New Playlist</span>
+                            </div>
+                        </div>
+                    </div>
+                    {myPlaylists.map((playlist) => (
+                        <div key={playlist.id} className="opt03" onClick={handleAddToPlaylist.bind({ id: playlist.id })}>
+                            {playlist.name}
+                        </div>
+                    ))}
+                </>
+            )}
         </div>
     );
 };
