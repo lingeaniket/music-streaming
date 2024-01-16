a028170dead67c96177bfe27dcb2bc65f3b436a2
lingeaniket <linge.aniket.10@gmail.com>
2024-01-16 17:13:03 +0530

    usertab updated

--- src/Components/UserTab/UserTab.js
+++ src/Components/UserTab/UserTab.js
@@ -4,18 +4,46 @@ import { useSelector } from "react-redux";
 import "./userTab.css";
 import { useParams } from "react-router-dom";
 import SongListMain from "./SongList/SongListMain";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
+import Options from "../Options/Options";
 
 const UserTab = () => {
     const { id } = useParams();
     const playlists = useSelector((state) => state.playlist.myPlaylists);
-    
+
     const [imageData, setImageData] = useState([]);
     const [currentLoad, setCurrentLoad] = useState(20);
     const [currentList, setCurrentList] = useState({});
+    const [options, setoptions] = useState(false);
+
     const [currentSongs, setCurrentSongs] = useState([]);
 
     const loadMoreSongs = () => {
         setCurrentLoad((prev) => prev + 20);
+    };
+
+    const handleOptions = (event) => {
+        const { top, right } = event.target.parentNode.getBoundingClientRect();
+        const parent = event.target.closest(".options");
+
+        const option = parent.querySelector(".options01");
+
+        if (top > window.innerHeight / 2) {
+            option.style.bottom = "10%";
+        } else {
+            option.style.top = "100%";
+        }
+
+        if (right < window.innerWidth / 2) {
+            option.style.left = "25%";
+        } else {
+            option.style.right = "10%";
+        }
+
+        setTimeout(() => {
+            setoptions(true);
+        }, 0);
     };
 
     useEffect(() => {
@@ -67,6 +95,20 @@ const UserTab = () => {
                     </div>
                     <div>
                         {currentList?.songs?.length} <span>{currentList?.songs?.length > 1 ? "songs" : "song"}</span>
+                    </div>
+                    <div className="detail-09 app01">
+                        <div className="detail-10">
+                            <button>Play</button>
+                        </div>
+
+                        <div className="detail-11 options">
+                            <button className="app05" onClick={handleOptions}>
+                                <FontAwesomeIcon icon={faEllipsis} size="2xl" style={{ color: "#ffffff" }} />
+                            </button>
+                            <div className="options01">
+                                {options && <Options playlist={true} data={currentList} options={options} setoptions={setoptions} />}
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
