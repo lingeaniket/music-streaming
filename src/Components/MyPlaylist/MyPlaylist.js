d1461d038938cf98c8216c8cf7a61b2c21671db3
lingeaniket <linge.aniket.10@gmail.com>
2024-01-22 12:17:03 +0530

    MyPlaylist.js updated

--- src/Components/MyPlaylist/MyPlaylist.js
+++ src/Components/MyPlaylist/MyPlaylist.js
@@ -3,12 +3,12 @@ import { useSelector } from "react-redux";
 import PlaylistItem from "./PlaylistItem";
 import "./myPlaylist.css";
 
-const MyPlaylist = () => {
+const MyPlaylist = ({ handleClose }) => {
     const playlists = useSelector((state) => state.playlist.myPlaylists);
     return (
         <div className="mplay04">
             {playlists.map((playlist) => (
-                <PlaylistItem key={playlist.id} playlist={playlist} />
+                <PlaylistItem handleClose={handleClose} key={playlist.id} playlist={playlist} />
             ))}
         </div>
     );
