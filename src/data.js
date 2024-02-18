314d0c26e6f268df906e50db392bef8b36875272
lingeaniket <linge.aniket.10@gmail.com>
2024-02-18 12:20:13 +0530

    data changes

--- src/data.js
+++ src/data.js
@@ -1,32 +1,5 @@
-// to get auth code
 
-// var client_id = "7bb900aae1c544518c151201106d7bba";
-//         var client_secret = "efd828bfe750468d826269be822295ce";
-
-//         const authOptions = {
-//             method: "post",
-//             url: "https://accounts.spotify.com/api/token",
-//             headers: {
-//                 Authorization: "Basic " + btoa(`${client_id}:${client_secret}`),
-//                 "Content-Type": "application/x-www-form-urlencoded",
-//             },
-//             data: new URLSearchParams({
-//                 grant_type: "client_credentials",
-//             }),
-//         };
-
-//         axios(authOptions)
-//             .then((response) => {
-//                 // Handle the successful response here
-//                 const token = response.data;
-//                 console.log("Access Token:", token);
-//             })
-//             .catch((error) => {
-//                 // Handle errors here
-//                 console.error("Error:", error.response.data);
-//             });
-
-//yiu may like for album
+//you may like for album
 // https://www.jiosaavn.com/api.php?__call=reco.getAlbumReco&api_version=4&_format=json&_marker=0&ctx=wap6dot0&albumid=49986025
 
 // related playlist
