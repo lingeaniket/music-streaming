42c1c6446b48d7df1ff6bc91a221df4e76f81bb5
lingeaniket <linge.aniket.10@gmail.com>
2024-03-15 12:53:24 +0530

    list updated

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -26,6 +26,7 @@ const LandingPage = () => {
                     .map((key) => musicData[key])
                     .filter((item) => item.title && ["album", "playlist", "chart", "song"].includes(item.data[0].type));
                 setLoadingData(sortedArray);
+                console.log(sortedArray);
                 setLoading(false);
             });
         };
--- src/Components/LandingPage/List/List.js
+++ src/Components/LandingPage/List/List.js
@@ -6,9 +6,12 @@ import ListItem from "./ListItem/ListItem";
 const List = ({ data }) => {
     return (
         <div className="list01 app01 list10">
-            {data?.map((val, i) => (
-                <ListItem key={i} data={val} />
-            ))}
+            {data?.map((val, i) => {
+                if (["album", "playlist", "chart", "song"].includes(val.type)) {
+                    return <ListItem key={i} data={val} />;
+                }
+                return <></>;
+            })}
         </div>
     );
 };
